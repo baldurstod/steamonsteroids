170f90cbf430142153a4e4dcfc71d54c063c2d2b
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-18 10:47:27 +0200

    Added constants.ts

---
+++ src/client/ts/constants.ts
@@ -0,0 +1,5 @@
+
+export const TF2_REPOSITORY = 'https://tf2content.loadout.tf/';
+export const CSGO_REPOSITORY = 'https://cs2content.csloadout.com/';
+
+export const TF2_WARPAINT_DEFINITIONS_URL = TF2_REPOSITORY + 'generated/warpaint_definitions.json';
