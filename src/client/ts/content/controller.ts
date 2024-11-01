03fc1e967a1b32fc8667d0a79628fb45944010e1
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-11-02 00:08:55 +0100

    Added convenience methods

--- src/client/ts/content/controller.ts
+++ src/client/ts/content/controller.ts
@@ -1,1 +1,13 @@
+export enum ControllerEvents {
+	Tf2RefreshListing = 'refreshlisting',
+}
+
 export const Controller = new EventTarget();
+
+export function controllerDispatchEvent(type: ControllerEvents, options?: CustomEventInit) {
+	Controller.dispatchEvent(new CustomEvent(type, options));
+}
+
+export function controlleraddEventListener(type: ControllerEvents, callback: EventListenerOrEventListenerObject, options?: AddEventListenerOptions): void {
+	Controller.addEventListener(type, callback, options);
+}
