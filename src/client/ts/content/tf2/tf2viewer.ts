c3633057572fc9677721b5a6e085c5913e2dc57e
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-11-01 23:37:18 +0100

    Fixed typescript declarations

--- src/client/ts/content/tf2/tf2viewer.ts
+++ src/client/ts/content/tf2/tf2viewer.ts
@@ -1,10 +1,12 @@
 
-import { createElement } from 'harmony-ui';
+import { createElement, HTMLHarmonyToggleButtonElement } from 'harmony-ui';
 import { Camera, Group, PointLight, Repositories, Repository, RotationControl, Scene, Source1ModelInstance } from 'harmony-3d';
 import { DECORATED_WEAPONS, TF2_REPOSITORY } from '../../constants';
 import { TextureCombiner, WeaponManagerEventTarget } from 'harmony-3d-utils';
-import { Controller } from '../controller';
+import { Controller, controllerDispatchEvent, ControllerEvents } from '../controller';
 import { sortSelect } from '../utils/sort';
+import { pauseSVG, playSVG } from 'harmony-svg';
+import 'harmony-ui/dist/define/harmony-toggle-button.js';
 
 enum TeamColor {
 	RED = 0,
@@ -17,8 +19,6 @@ enum GenerationState {
 	Failure,
 }
 
-
-
 export class TF2Viewer {
 	#classModels = new Map<string, Source1ModelInstance>();
 	#scene = new Scene();
@@ -29,8 +29,8 @@ export class TF2Viewer {
 	#teamColor: TeamColor = TeamColor.RED;
 	#htmlControls?: HTMLElement;
 	#htmlWeaponSelector?: HTMLSelectElement;
-	#htmlClassIcons?: HTMLSelectElement;
-	#forcedWeaponIndex: string = '';
+	#htmlClassIcons?: HTMLElement;
+	#forcedWeaponIndex: number | null = null;
 	constructor() {
 		Repositories.addRepository(new Repository('tf2', TF2_REPOSITORY));
 		//WeaponManager.reuseTextures = true;
@@ -53,57 +53,81 @@ export class TF2Viewer {
 		this.#htmlControls = document.createElement('div');
 		this.#htmlControls.className = 'canvas-container-controls';
 
-		this.#htmlWeaponSelector = document.createElement('select');
-		this.#htmlWeaponSelector.className = 'weapon-selector';
-		this.#htmlWeaponSelector.addEventListener('change', event => {
-			this.#forcedWeaponIndex = (event.target as HTMLSelectElement).value;
-			chrome.storage.sync.set({ warpaintWeaponIndex: (event.target as HTMLSelectElement).value });
-			this.#refreshListing();
-		});
+		this.#htmlWeaponSelector = createElement('select', {
+			parent: this.#htmlControls,
+			class: 'weapon-selector',
+			events: {
+				change: (event: Event) => {
+					this.#forcedWeaponIndex = Number((event.target as HTMLSelectElement).value);
+					chrome.storage.sync.set({ warpaintWeaponIndex: (event.target as HTMLSelectElement).value });
+					this.#refreshListing();
+				},
+			}
+		}) as HTMLSelectElement;
 
 		for (let weaponName in DECORATED_WEAPONS) {
 			let weaponDefIndex = DECORATED_WEAPONS[weaponName];
 			let weaponOption = document.createElement('option');
 			weaponOption.innerHTML = weaponName;
-			weaponOption.value = weaponDefIndex;
+			weaponOption.value = String(weaponDefIndex);
 			this.#htmlWeaponSelector.appendChild(weaponOption);
 		}
 		sortSelect(this.#htmlWeaponSelector);
 
-		this.#htmlClassIcons = document.createElement('div');
-		this.#htmlClassIcons.className = 'canvas-container-controls-class-icons';
+		this.#htmlClassIcons = createElement('div', {
+			parent: this.#htmlControls,
+			class: 'canvas-container-controls-class-icons',
+		}) as HTMLElement;
 
-		let htmlPlayPauseButton = createElement('button', {
+		const htmlPlayPauseButton: HTMLHarmonyToggleButtonElement = createElement('harmony-toggle-button', {
 			class: 'canvas-container-controls-playpause play',
-			innerHTML: pauseSVG,
-			parent: this.htmlControls,
+			//innerHTML: pauseSVG,
+			parent: this.#htmlControls,
+			childs: [
+				createElement('on', { innerHTML: playSVG }),
+				createElement('off', { innerHTML: pauseSVG }),
+			],
 			events: {
 				click: () => {
-					htmlPlayPauseButton.buttonState = !htmlPlayPauseButton.buttonState
-					if (htmlPlayPauseButton.buttonState) {
+					//htmlPlayPauseButton.setAttribute('buttonState', ) = !htmlPlayPauseButton.buttonState
+					if (htmlPlayPauseButton.state) {
 						this.#rotationControl.rotationSpeed = 1;
-						htmlPlayPauseButton.innerHTML = pauseSVG;
+						//htmlPlayPauseButton.innerHTML = pauseSVG;
 					} else {
 						this.#rotationControl.rotationSpeed = 0;
-						htmlPlayPauseButton.innerHTML = playSVG;
+						//htmlPlayPauseButton.innerHTML = playSVG;
 					}
 				}
 			},
 			buttonState: true,
-		});
+		}) as HTMLHarmonyToggleButtonElement;
 
 
-		this.loadWarpaintWeapon();
-		this.htmlControls.append(this.htmlWeaponSelector, this.htmlClassIcons);
-		return this.htmlControls;
+		this.#loadWarpaintWeapon();
+		return this.#htmlControls;
 	}
 
 	async #refreshListing() {
+		//Controller.dispatchEvent(new Event(ControllerEvents.Tf2RefreshListing, )
+		controllerDispatchEvent(ControllerEvents.Tf2RefreshListing);
+		/*
 		if (this.application.isMarketPage) {
 			await this.application.renderListing(this.application.currentListingId, true);
 		}
 		if (this.application.isInventoryPage) {
 			await this.application.renderInventoryListing(this.application.currentAppId, this.application.currentContextId, this.application.currentAssetId, undefined, true);
+		}
+			*/
+	}
+
+	async #loadWarpaintWeapon() {
+		let storage = await chrome.storage.sync.get('warpaintWeaponIndex');
+		if (storage && storage.warpaintWeaponIndex) {
+			let warpaintWeaponIndex = Number(storage.warpaintWeaponIndex);
+			if (this.#htmlWeaponSelector) {
+				this.#htmlWeaponSelector.value = String(warpaintWeaponIndex);
+			}
+			this.#forcedWeaponIndex = warpaintWeaponIndex;
 		}
 	}
 }
