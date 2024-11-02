17c08132c269939cf4702b7a3ee2ae305a0f97d6
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-11-02 22:15:52 +0100

    Added inspect.ts

---
+++ src/client/ts/content/utils/inspect.ts
@@ -0,0 +1,10 @@
+export function getInspectLink(listingDatas: any/*TODO: change type*/, listingOrSteamId: number, assetId?: number) {
+	if (listingDatas.actions) {
+		for (let action of listingDatas.actions) {
+			let link = action.link;
+			if (link && link.startsWith('steam://rungame/')) {
+				return link.replace('%listingid%', listingOrSteamId).replace('%owner_steamid%', listingOrSteamId).replace('%assetid%', assetId ?? listingDatas.id);
+			}
+		}
+	}
+}
