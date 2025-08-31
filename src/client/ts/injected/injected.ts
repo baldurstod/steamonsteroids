140318fcded074c548ea0ede0e8ca213de9ccddc
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-08-31 16:41:36 +0200

    Injected: added proper declaration for steam properties

--- src/client/ts/injected/injected.ts
+++ src/client/ts/injected/injected.ts
@@ -2,12 +2,12 @@ window.addEventListener('message', event => {
 	let messageData = event.data;
 	switch (messageData.action) {
 		case 'requestListingAssets':
-			window.postMessage({action: 'responseListingAssets', listingAssets: (window as any).g_rgAssets, promiseId:messageData.promiseId}, '*');
+			window.postMessage({action: 'responseListingAssets', listingAssets: window.g_rgAssets, promiseId:messageData.promiseId}, '*');
 			break;
 		case 'requestListingAsset':
 			let asset;
 			try {
-				asset = (window as any).g_rgAssets[messageData.appId][messageData.contextId][messageData.assetId];
+				asset = window.g_rgAssets[messageData.appId][messageData.contextId][messageData.assetId];
 			} catch (e) {}
 			if (asset) {
 				delete asset.$row;//delete the junk added by SIH
@@ -15,7 +15,7 @@ window.addEventListener('message', event => {
 			window.postMessage({action: 'responseListingAsset', listingAssetDatas: asset, promiseId:messageData.promiseId}, '*');
 			break;
 		case 'requestListingInfo':
-			window.postMessage({action: 'responseListingInfo', listingInfo: (window as any).g_rgListingInfo, promiseId:messageData.promiseId}, '*');
+			window.postMessage({action: 'responseListingInfo', listingInfo: window.g_rgListingInfo, promiseId:messageData.promiseId}, '*');
 			break;
 		case 'requestInventoryAssetDatas':
 			let description;
@@ -23,10 +23,10 @@ window.addEventListener('message', event => {
 				let appId = messageData.appId;
 				let contextId = messageData.contextId;
 				let assetId = messageData.assetId;
-				description = (window as any).g_rgAppContextData?.[appId]?.rgContexts?.[contextId]?.inventory?.m_rgAssets?.[assetId]?.description;
+				description = window.g_rgAppContextData?.[appId]?.rgContexts?.[contextId]?.inventory?.m_rgAssets?.[assetId]?.description;
 				if (!description) {
 					// We are in a trade window. Search the asset in both inventories
-					description = (window as any).UserYou?.rgAppInfo?.[appId]?.rgContexts?.[contextId]?.inventory?.rgInventory?.[assetId] ?? (window as any).UserThem?.rgAppInfo?.[appId]?.rgContexts?.[contextId]?.inventory?.rgInventory?.[assetId];
+					description = window.UserYou?.rgAppInfo?.[appId]?.rgContexts?.[contextId]?.inventory?.rgInventory?.[assetId] ?? window.UserThem?.rgAppInfo?.[appId]?.rgContexts?.[contextId]?.inventory?.rgInventory?.[assetId];
 				}
 			} catch (e) {}
 			if (description) {
@@ -41,26 +41,26 @@ window.addEventListener('message', event => {
 			}
 			break;
 		case 'requestInventorySteamId':
-			window.postMessage({action: 'responseInventorySteamId', steamId: (window as any).g_ActiveUser?.strSteamId, promiseId:messageData.promiseId}, '*');
+			window.postMessage({action: 'responseInventorySteamId', steamId: window.g_ActiveUser?.strSteamId, promiseId:messageData.promiseId}, '*');
 			break;
 		case 'setInventoryFilter':
-			if ((window as any).g_ActiveInventory && (window as any).g_ActiveInventory.LoadCompleteInventory) {
-				(window as any).g_ActiveInventory.LoadCompleteInventory().done(() => (window as any).Filter.UpdateTagFiltering(messageData.filter));
+			if (window.g_ActiveInventory && window.g_ActiveInventory.LoadCompleteInventory) {
+				window.g_ActiveInventory.LoadCompleteInventory().done(() => window.Filter.UpdateTagFiltering(messageData.filter));
 			}
 			break;
 		case 'activeInventorySetActivePage':
-			if ((window as any).g_ActiveInventory) {
-				let page = Math.max(Math.min(messageData.page, (window as any).g_ActiveInventory.pageTotal), 1);
-				(window as any).g_ActiveInventory.SetActivePage(page - 1)
+			if (window.g_ActiveInventory) {
+				let page = Math.max(Math.min(messageData.page, window.g_ActiveInventory.pageTotal), 1);
+				window.g_ActiveInventory.SetActivePage(page - 1)
 			}
 			break;
 		case 'AjaxPagingControlsGoToPage':
 			let controlName = messageData.name;
 			if (controlName) {
-				let control = window[controlName];
+				let control = window[controlName] as any;
 				if (control) {
-					let page = Math.max(Math.min(messageData.page, (control as any).m_cMaxPages), 1);
-					(control as any).GoToPage(page - 1);
+					let page = Math.max(Math.min(messageData.page, control.m_cMaxPages), 1);
+					control.GoToPage(page - 1);
 				}
 			}
 			break;
