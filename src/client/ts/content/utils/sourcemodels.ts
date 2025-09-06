8e8a8919711d4e32f43cd0baa0c7801a8b5c8120
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-09-06 11:38:58 +0200

    addSource1Model(): parent parameter is now optional

--- src/client/ts/content/utils/sourcemodels.ts
+++ src/client/ts/content/utils/sourcemodels.ts
@@ -1,9 +1,9 @@
 import { Entity, Source1ModelInstance, Source1ModelManager } from 'harmony-3d';
 
-export async function addSource1Model(repository: string, path: string, parent: Entity): Promise<Source1ModelInstance | null> {
+export async function addSource1Model(repository: string, path: string, parent?: Entity): Promise<Source1ModelInstance | null> {
 	let model = await Source1ModelManager.createInstance(repository, path, true);
 	if (model) {
-		parent.addChild(model);
+		parent?.addChild(model);
 		model.frame = 0.;
 	}
 	return model;
